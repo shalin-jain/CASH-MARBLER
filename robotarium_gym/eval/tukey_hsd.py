4a2f4ee3acd3da82af67159302df361222ad5091
Shalin Jain <sjain441@gatech.edu>
2025-01-28 17:43:07 -0500

    fix SD results

--- robotarium_gym/eval/tukey_hsd.py
+++ robotarium_gym/eval/tukey_hsd.py
@@ -30,7 +30,7 @@ def tukey_hsd_test(x, y, z, metric_name):
     print(tukey_result.pvalues)
 
 if __name__ == "__main__":
-    parent_folder = "PCP_SF/"
+    parent_folder = "PCP_SD/"
     scenario = "PredatorCapturePrey"
     # load data per method
     hyper_rnn = load_pkl(f"{parent_folder}HyperRNNAgent_True_{scenario}/metrics.pkl")
